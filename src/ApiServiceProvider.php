3774c77e1b2d3eb8919ad6f5fb674e6466b4349d
yansongda <me@yansongda.cn>
2018-01-10 23:55:35 +0800

    i

--- src/ApiServiceProvider.php
+++ src/ApiServiceProvider.php
@@ -8,13 +8,52 @@ use Laravel\Lumen\Application as LumenApplication;
 
 class PayServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap.
+     *
+     * @author yansongda <me@yansonga.cn>
+     *
+     * @return void
+     */
     public function boot()
+    {
+        $this->loadMigrationsFrom(dirname(__DIR__).'/migrations');
+
+        $this->loadRoutesFrom(dirname(__DIR__).'/routes/api.php');
+        $this->loadRoutesFrom(dirname(__DIR__).'/routes/admin.php');
+
+        $this->loadViewsFrom(dirname(__DIR__).'/views', 'laravelApi');
+
+        $this->publishResources();
+    }
+
+    /**
+     * Registe the service.
+     *
+     * @author yansongda <me@yansonga.cn>
+     *
+     * @return void
+     */
+    public function register()
     {
         # code...
     }
 
-    public function register()
+    /**
+     * Publish resouces.
+     *
+     * @author yansongda <me@yansonga.cn>
+     *
+     * @return void
+     */
+    public function publishResources()
     {
-        # code...
+        $this->publishes([
+            dirname(__DIR__).'/views' => resource_path('views/vendor/api'),
+        ], 'laravel-api-config');
+
+        $this->publishes([
+            dirname(__DIR__).'/migrations' => database_path('migrations')
+        ], 'laravel-api-migrations');
     }
 }
